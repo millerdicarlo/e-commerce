6827897837e191e929eb7096960574b98a2d9e3b
millerd <millerdicarlo@gmail.com>
2022-10-05 15:49:47 -0400

    forgot to coomit for awhile so I've added mysql2, dotenv, etc

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,3 +1,30 @@
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+module.exports = sequelize;
+
+
+
+
+
+
+
+
+
+
+
+
+// Sample code to copare for bugs
+/*
 require('dotenv').config();
 
 const Sequelize = require('sequelize');
@@ -13,3 +40,19 @@ const sequelize = process.env.JAWSDB_URL
     });
 
 module.exports = sequelize;
+
+
+//PART 2
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+
+// create connection to our database, pass in your MySQL information for username and password
+const sequelize = new Sequelize('just_tech_news_db', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+module.exports = sequelize;
+
+*/
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # E-commerce Back End Starter Code
+
+## Purpose
+This application 
+
+## Built Using
+* Node.js
+* MySQL
+* JavaScript
+* Inquirer
+* Express.js
+
+# Website
+
+
+## Contribution
+Made by Miller DiCarlo
